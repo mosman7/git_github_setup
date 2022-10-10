85152704c1af17f210d4358eab74a93316fb7ee1
mosman7 <mohamedosman998@hotmail.com>
2022-10-10 17:39:07 +0100

    updated

---
+++ README.md
@@ -0,0 +1,26 @@
+# Python intro
+## Why Python
+### Python use cases
+#### Python
+
+`print ("Hello World")`
+``` python
+first_name = "Mohamed"
+last_name = "Osman"
+DOB = "05/05/1998"
+Course_name = "Eng130"
+uk_resident = bool
+
+
+print("Hello, What is your name")
+first_name = input()
+print("Please enter your last name")
+last_name = input()
+print("Nice to meet you", first_name, last_name)
+d
+print("What is your date of birth")
+DOB = input()
+
+print("Are you a UK resident?")
+uk_resident = input()
+```
---
+++ hello_world.py
@@ -0,0 +1,34 @@
+
+#print("hello world")
+#Variables
+#- Used to store any data / user date
+#-anything in quotation marks is a string
+#-any number is a integer
+#-yes or no is Boolean
+#-if you have a number with a decimal it is a float
+
+#How to find out the type of data stored
+#-type()
+#-print(type(variable name))
+
+# - comment
+# comment multiple lines at once ctrl + /
+
+first_name = "Mohamed"
+last_name = "Osman"
+DOB = "05/05/1998"
+Course_name = "Eng130"
+uk_resident = bool
+
+
+print("Hello, What is your name")
+first_name = input()
+print("Please enter your last name")
+last_name = input()
+print("Nice to meet you", first_name, last_name)
+
+print("What is your date of birth")
+DOB = input()
+
+print("Are you a UK resident?")
+uk_resident = input()
