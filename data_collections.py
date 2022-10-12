c3e180c18f9e4131f3680fbc2e5a449d3582d85e
mosman7 <mohamedosman998@hotmail.com>
2022-10-12 12:26:20 +0100

    deleted

--- data_collections.py
+++ data_collections.py
@@ -12,20 +12,25 @@
 # print(type(shopping_list))
 # #how to add to a list
 # shopping_list.append("chicken")
-# print(shopping_list[3]) # prints the value in index 3
-# shopping_list[2] = "ice cream" # replaces in the list
+# print(shopping_list[3])               # prints the value in index 3
+# shopping_list[2] = "ice cream"        # replaces in the list
 # print(shopping_list)
 # shopping_list.pop()
-# print(shopping_list) #deletes the last item in the list
-# shopping_list.remove("fanta") # removes from the list
+# print(shopping_list)                  #deletes the last item in the list
+# shopping_list.remove("fanta")         # removes from the list
 # print(shopping_list)
 
 #Tuples
 # Cannot be changes, added to or edited
 # such as user details, DOB, etc
+#
+# essentials = ("milk", "water")
+# print(essentials)
+# print(type(essentials))
+# #essentials [0] = "coffee"
+# print(essentials)
 
-essentials = ("milk", "water")
-print(essentials)
-print(type(essentials))
-#essentials [0] = "coffee"
-print(essentials)
+if 5 > 2:
+ #   print("Five is greater than two!")
+
+    print(bool(0))
---
+++ dict.py
@@ -0,0 +1,66 @@
+# Dictionary - data collection type
+#syntax = {"name": "sparta"}
+#         {   }... and so on
+# store student data: name, course_name, progress, completed_lessons
+student_1 = {
+#   "keys": "values"
+    "name": "Mohamed",
+    "Course_name": "Eng130",
+    "progress": "Good",
+    "completed_tasks": "4",
+    "completed_lessons": ["lists", "tuples", "operators"]
+}
+print(type(student_1))                  # prints type
+print(student_1["name"])                # prints the value of stated key
+print(student_1)                        # prints dictionary
+print(student_1["completed_lessons"])
+print(student_1["completed_lessons"][0])
+
+student_1["completed_tasks"] = 2            #changes value of completed tasks
+print(student_1["completed_tasks"])
+
+student_1["completed_lessons"][2] = "OOP"   # changes index 2 value
+print(student_1["completed_lessons"])
+
+# delete an item from the list
+student_1["completed_lessons"].remove("OOP") #deletes value
+print(student_1["completed_lessons"])
+
+#dict builtin methods
+# can display only keys or values
+print(student_1.keys())
+
+
+# Dictionary basics :D
+
+#1 - Define a dictionary call story1, it should have the followign keys:
+        # 'start', 'middle' and 'end'
+
+story1 = {
+    "start": "I was in the shopping centre and i met a beautiful lady",
+    "middle": "we spoke and had a coffee date",
+    "end": "we got married and lived happily ever after"
+}
+
+#2 - Print the entire dictionary
+print(story1)
+
+#3 - Print the type of your dictionary
+print(type(story1))
+
+#4 - Print only the keys
+print(story1.keys())
+
+#5 - print only the values
+print(story1.values())
+
+#6 - print the individual values using the keys (individually, lots of printi commands)
+print(story1["start"])
+print(story1["middle"])
+print(story1["end"])
+
+#7 - Now let's add a new key:value pair.
+    # 'hero': yourSuperHero
+
+story1["hero"]= "Mr Shahrukh"
+print(story1)
